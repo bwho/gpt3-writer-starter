925a5cce092d5e4b1cf31d64a739ca0e45204226
bwho <barrett.olafson@gmail.com>
2022-12-28 16:49:26 -0500

    latest build

---
+++ pages/api/generate.js
@@ -0,0 +1,29 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration ({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix =
+`
+Write a step-by-step recipe that is plant-based and includes at least one of the following ingredients. The ideal output will include all ingredients. Provide a name for the recipe in a section titled "Recipe Title:". In a bulleted list, provide the list of ingredients and their amounts in a section titled "Ingredients:". In a sub section of "Ingredients:" titled "Additional Ingredients:", identify all of the additional ingredients that are needed to complete the recipe that were not included in the initially submitted ingredients. The additional ingredients needed should be inexpensive and usually found at home already. If applicable, include a section called "Time:" that notes both the "Prep Time:" and "Cook Time". Include step-by-step instructions on how to complete the recipe in a section titled "Instructions:".
+
+Ingredients:
+`;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userCombinedInput}\n\n`,
+    temperature: 0.5,
+    max_tokens: 256,
+  });
+
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,97 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
+
 
 const Home = () => {
+  const [userVeggieInput, setUserVeggieInput] = useState('');
+  const [userAdditionalInput, setUserAdditionalInput] = useState('');
+
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    
+    const userCombinedInput = `${userVeggieInput}, ${userAdditionalInput}`;
+    
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userCombinedInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+
+  const onUserChangedVeggieText = (event) => {
+    setUserVeggieInput(event.target.value);
+  };
+
+  const onUserChangedAdditionalText = (event) => {
+    setUserAdditionalInput(event.target.value);
+  };
+
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>Plant-Based Recipe Generator | olafsonfarms</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Plant-Based Recipe Generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Use AI to generate plant-based recipes based on the ingredients you grow in your backyard garden.</h2>
           </div>
+        </div>
+        <div className="prompt-container">
+          <h3>Enter a comma-separated list of the ingredients that you can harvest today:</h3> 
+          <textarea 
+            className="prompt-box" 
+            placeholder="Write your list here..."
+            value={userVeggieInput}
+            onChange={onUserChangedVeggieText}
+          />
+          <span/>
+          <span/>
+          <h3>And add any additional ingredients that you'd like to use from home:</h3>
+          <textarea 
+            className="prompt-box" 
+            placeholder=""
+            value={userAdditionalInput}
+            onChange={onUserChangedAdditionalText}
+          />
+          <div className="prompt-buttons">
+            <a
+              className={isGenerating ? 'generate-button loading' : 'generate-button'}
+              onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
+              {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+              </div>
+            </a>
+          </div>
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Your Custom Recipe</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
         </div>
       </div>
       <div className="badge-container grow">
